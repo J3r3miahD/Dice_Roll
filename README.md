d0f1c4581ba79299f7dd7a357382f53193acf8b6
Jeremiah Davis <jdmiah@gmail.com>
2025-01-12 19:05:29 -0500

    Commit with changes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-Program simulates rolling dice.
-Ask for user input and return results.
+Welcome to the Dice Rolling Simulator!
+
+This program simulates rolling a dice multiple times. Here's how it works:
+
+1. You will be asked to enter the number of sides on the dice (e.g., 6 for a standard die).
+2. You will also be asked how many times you want to roll the dice.
+3. The program will then roll the dice the specified number of times, showing the result of each roll.
+
+The dice rolls are random, and each roll is independent of the previous ones.
+
+Let's get started!
+
+Enter the number of sides on the dice (e.g., 6 for a typical die): 8
+Enter the number of times to roll the dice: 5
+Rolling a 8-sided dice 5 times...
+
+Roll 1: 3
+Roll 2: 7
+Roll 3: 2
+Roll 4: 5
+Roll 5: 8
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from random import randint
 
-def roll_dice(sides, rolls):
-    print(f"Rolling {sides}-sided dice {rolls} times...\n")
-    for i in range(1, rolls + 1):
-        roll = randint(1, sides)
+def print_instructions():
+    print("""
+    Welcome to the Dice Rolling Simulator!
+
+    This program simulates rolling a dice multiple times. Here's how it works:
+
+    1. You will be asked to enter the number of sides on the dice (e.g., 6 for a standard die).
+    2. You will also be asked how many times you want to roll the dice.
+    3. The program will then roll the dice the specified number of times, showing the result of each roll.
+
+    The dice rolls are random, and each roll is independent of the previous ones.
+
+    Let's get started!
+    """)
+
+def roll_dice(num_sides, num_rolls):
+    print(f"Rolling a {num_sides}-sided dice {num_rolls} times...\n")
+    for i in range(1, num_rolls + 1):
+        roll = randint(1, num_sides)
         print(f"Roll {i}: {roll}")
 
-
 if __name__ == '__main__':
-    sides = int(input("Enter the number of sides on the dice: "))
+    print_instructions()
+
+    sides = int(input("Enter the number of sides on the dice (e.g., 6 for a typical die): "))
     rolls = int(input("Enter the number of times to roll the dice: "))
 
     roll_dice(sides, rolls)
